bfb453adc910dc6b6bbbaf30ab44c36109e0730f
Dr3xler <sebastian.ewald1@unseen.is>
2015-11-05 20:47:17 +0100

    Update takemyenergie.c

--- takemyenergie.c
+++ takemyenergie.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
   int main() {
   
-    char text[500]
-    
-    printf("Hey there, i'm a programm which'll give you energy. Just write below, whats the difference between a rabbit.");
-    scanf("%s", text)
-    
-    If(%s=='the Rabbit'):
-      printf("Your answer is right. \n TAKE \n MY \n ENERGYYYYYY");
-      
-      elseif() 
-      return -1;
-      
-    }
+   printf("beispielcode :P");
+  }
